94d758046dd49dfa283183ac13bde2feebc58713
utsav subedi <subediutsav8@gmail.com>
2022-03-16 21:43:28 +0545

    react blog initialized

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import NavBar from './Navbar';
+import Home from './Home';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <h1>New Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
