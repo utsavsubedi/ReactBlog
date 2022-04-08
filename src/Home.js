460b6c04f265122514f17f6be5d0b82fec87d814
utsav subedi <subediutsav8@gmail.com>
2022-04-08 22:32:56 +0545

    css for front page, use state, function calls

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('ram');
+    const [age, setAge] = useState(20);
+    const handleClick = () => {
+        setName('shyam');
+        setAge('30');
+    }
+
     return (
         <div className="home">
             <h1>Home</h1>
+            <p>{name} is {age} years old.</p>
+            <div>
+                <button onClick={ handleClick }>Click Me</button>
+            </div>
         </div>
     );
 }
