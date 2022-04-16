3c7db239b84204e951da194012f25889461cac62
utsav subedi <subediutsav8@gmail.com>
2022-04-16 14:50:37 +0545

    create blog form

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 function CreateBlog(){
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [aurthor, setAurthor] = useState('');
     return (
         <div className= "create-box">
             <h1>Create blog</h1>
+            <form>
+                <label>Blog Title: </label>
+                <input 
+                    type="text"
+                    required
+                    value = {title}
+                    onChange= {(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Content: </label>
+                <textarea
+                required 
+                value ={content}
+                onChange = {(e) => setContent(e.target.value)}
+                >    
+                </textarea>
+                <label>Blog Aurthor: </label>
+                <input 
+                    type="text"
+                    onChange = {(e) => setAurthor(e.target.value)}
+                />
+                <button>Add Blog</button>
+            </form>
+            <p>{title}</p>
+            <p>{aurthor}</p>
+            <p>{content}</p>
         </div>
     )
 };
