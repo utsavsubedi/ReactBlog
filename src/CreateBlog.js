e9277fe75035cbe0869f98e621539dcca1a95ab2
utsav subedi <subediutsav8@gmail.com>
2022-04-16 17:49:02 +0545

    post request

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,13 +1,32 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 function CreateBlog(){
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const [aurthor, setAurthor] = useState('');
+    const [aurthor, setAurthor] = useState('utsav');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, aurthor, content}
+        setIsPending(true);
+        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('New blog added');
+            setIsPending(false);
+            history.push('/');
+        })
+
+    }
     return (
         <div className= "create-box">
             <h1>Create blog</h1>
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <label>Blog Title: </label>
                 <input 
                     type="text"
@@ -23,11 +42,17 @@ function CreateBlog(){
                 >    
                 </textarea>
                 <label>Blog Aurthor: </label>
-                <input 
-                    type="text"
+                <select
+                    value={aurthor}
                     onChange = {(e) => setAurthor(e.target.value)}
-                />
-                <button>Add Blog</button>
+                >
+                    <option value="utsav">utsav</option>
+                    <option value="maybe utsav">maybe utsav</option>
+                    <option value="not utsav">not utsav</option>
+                    <option value="definitely utsav">definitely utsav</option>
+                </select>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding Blog</button>}
             </form>
             <p>{title}</p>
             <p>{aurthor}</p>
